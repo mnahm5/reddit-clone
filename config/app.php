16048505c93503ef1c8bd8a55d03af9aa8a0b030
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-07-09 01:21:53 +1000

    Fractal basics

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
+        Spatie\Fractal\FractalServiceProvider::class,
 
         /*
          * Application Service Providers...
